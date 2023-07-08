c7aba1f8c96472af1d20d539758bcde85bf49a66
Hafiizh Ghulam <hafiizh.ghulam@frisidea.com>
2023-07-08 23:28:16 +0700

    improvements

--- app/Http/Controllers/Admin/ObservationController.php
+++ app/Http/Controllers/Admin/ObservationController.php
@@ -10,8 +10,11 @@ use App\Models\NotificationModel;
 use App\Models\ObservationHistoryModel;
 use App\Models\ObservationModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Str;
 
 class ObservationController extends Controller {
     protected $observationTable;
@@ -28,6 +31,14 @@ class ObservationController extends Controller {
         return ResponseHelper::response($observations);
     }
 
+    public function getApproved(Request $request) {
+        $observations = (new GeneralObservationController())->get([
+            "detail.status = " . ObservationStatusConstant::APPROVED
+        ]);
+
+        return ResponseHelper::response($observations);
+    }
+
     public function getDetail(Request $request, $id) {
         $validator = Validator::make([
             "id" => $id
@@ -36,12 +47,40 @@ class ObservationController extends Controller {
         ]);
         if ($validator->fails()) return ResponseHelper::response(null, $validator->errors()->first(), 400);
 
-        $observation = (new GeneralObservationController())->get([
-            "detail.status = " . ObservationStatusConstant::PENDING
-        ], $id);
+        $observation = ObservationModel::with("histories", "latestHistory")->find($id);
         if (empty($observation->id)) return ResponseHelper::response(null, "Observation not found", 400);
 
         return ResponseHelper::response($observation);
+    }
+
+    public function edit(Request $request) {
+        $validator = Validator::make($request->all(), [
+            "name" => "required|string",
+            "date" => "required|string|date",
+            "latitude" => ["required", "regex:/^[-]?(([0-8]?[0-9])\.(\d+))|(90(\.0+)?)$/"],
+            "longitude" => ["required", "regex:/^[-]?((((1[0-7][0-9])|([0-9]?[0-9]))\.(\d+))|180(\.0+)?)$/"],
+            "location" => "required|string",
+            "description" => "required|string"
+        ]);
+        if ($validator->fails()) return ResponseHelper::response(null, $validator->errors()->first(), 400);
+
+        return DB::transaction(function () use ($request) {
+            $observation = ObservationModel::find($request->id);
+            $observation->name = $request->name;
+            $observation->date = $request->date;
+            $observation->latitude = $request->latitude;
+            $observation->longitude = $request->longitude;
+            $observation->location = $request->location;
+            $observation->description = $request->description;
+            if ($request->hasFile("image")) {
+                $image = Carbon::now()->format("Y-m-d-H-i") . "-observation-" . Str::random(12) . "." . $request->file("image")->getClientOriginalExtension();
+                Storage::disk("public")->putFileAs("observation", $request->file("image"), $image);
+                $observation->image = $image;
+            }
+            $observation->save();
+
+            return ResponseHelper::response($observation);
+        });
     }
 
     public function set($id, $status) {
@@ -89,5 +128,18 @@ class ObservationController extends Controller {
 
     public function reject(Request $request, $id) {
         return $this->set($id, ObservationStatusConstant::REJECTED);
+    }
+
+    public function delete(Request $request, $id) {
+        $validator = Validator::make([
+            "id" => $id
+        ], [
+            "id" => "required|numeric|exists:$this->observationTable,id"
+        ]);
+        if ($validator->fails()) return ResponseHelper::response(null, $validator->errors()->first(), 400);
+
+        $observation = ObservationModel::find($id)->delete();
+
+        return ResponseHelper::response($observation);
     }
 }
--- routes/admin/observation.php
+++ routes/admin/observation.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get("get", "ObservationController@get");
+Route::get("get/approved", "ObservationController@getApproved");
 Route::get("get/detail/{id}", "ObservationController@getDetail");
 Route::get("approve/{id}", "ObservationController@approve");
 Route::get("reject/{id}", "ObservationController@reject");
+Route::post("edit", "ObservationController@edit");
+Route::delete("delete/{id}", "ObservationController@delete");
